afe24e6edda9e1a907da7d71f7c6efc7c3a14b23
Marina Khadieva <utabond89@gmail.com>
2021-10-21 01:19:29 +0300

    first commit

---
+++ 6/js/script.js
@@ -0,0 +1,14 @@
+let adminName;
+let userName;
+userName = prompt('Как зовут Юзера?', ' ');
+adminName = userName;
+
+let thirdSolarSystemPlanet = 'thirdSolarSystemPlanet'
+let humanityHomePlanet = 'humanityHomePlanet'
+let thirdPlanet = 'thirdPlanet'
+let onlyHabitablePlanet = 'onlyHabitablePlanet'
+
+let currentUser = 'currentUser'
+let currentSiteVisitor = 'currentSiteVisitor'
+
+let someName = 'Ilya';
