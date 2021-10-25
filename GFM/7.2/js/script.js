e9879b02e60dccb724cdd1b78d190cf2d0f74c76
Marina Khadieva <utabond89@gmail.com>
2021-10-25 21:54:50 +0300

    Неудачная попытка сделать задания по учебинку из главы про базовую математику

---
+++ GFM/7.2/js/script.js
@@ -0,0 +1,16 @@
+
+addEventListener("DOMContentLoaded", onReady); 
+
+function onReady() {
+    Prefixes;
+}
+
+function Prefixes(){
+let a = 1, b = 1;
+let d = b++;
+let c = ++a;
+let result1 = document.getElementById('result1');
+let result2 = document.getElementById('result2');
+result1.innerHTML = `// c = ${с} a = ${a}`; 
+return result2.innerHTML = `// b = ${b} d = ${d}`;
+}
