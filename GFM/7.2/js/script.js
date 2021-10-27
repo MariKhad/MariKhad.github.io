8bde8013a341c3398861fff761bc2cbc4278dc1a
Marina Khadieva <utabond89@gmail.com>
2021-10-27 22:28:12 +0300

    tourtures with toggles

--- GFM/7.2/js/script.js
+++ GFM/7.2/js/script.js
@@ -1,50 +1,38 @@
-
 addEventListener("DOMContentLoaded", onReady); 
 
 function onReady() {
-    prefixes();
-	 assigning();
-	 typeTransform();
-	 document.getElementById('launchAdd').onclick = correctAdition;
+     document.getElementById('x').oninput = up
+     document.getElementById('y').oninput = up
+	  document.getElementById('unar').onclick = getUnar;
+	  document.getElementById('binar').onclick = getBinar;
 }
 
-function prefixes(){
-let a = 1, b = 1;
-let d = b++;
-let c = ++a;
-let result1 = document.getElementById('result1');
-let result2 = document.getElementById('result2');
-result1.innerHTML = `// c = ${c} a = ${a}`; 
-result2.innerHTML = `// b = ${b} d = ${d}`;
+function up(e1) {
+	let elem = e1.target
+	elem.value = elem.value.replace(/\D/g, '')
 }
 
-function assigning(){
-	let a = 2;
-	let x = 1 + (a *= 2);
-	result3.innerHTML = `// a = ${a} x = ${x}`; 
+
+
+function getUnar() {
+	let x = document.getElementById('x').value;
+	let y = document.getElementById('y').value;
+	let result = document.getElementById('result');
+	let calc = x+y;
+	return result.innerHTML = `Вы сложили строки: ${calc}`;
 }
 
-function typeTransform() {
-result6.innerHTML = `// ${"" + 1 + 0}`; 
-result7.innerHTML = `// ${"" - 1 + 0}`; 
-result8.innerHTML = `// ${true + false}`; 
-result9.innerHTML = `// ${6 / "3"}`; 
-result10.innerHTML = `// ${"2" * "3"}`; 
-result11.innerHTML = `// ${4 + 5 + "px"}`; 
-result12.innerHTML = `// ${"$" + 4 + 5}`; 
-result13.innerHTML = `// ${"4" - 2}`; 
-result14.innerHTML = `// ${"4px" - 2}`; 
-result15.innerHTML = `// ${7 / 0}`; 
-result16.innerHTML = `// ${"  -9  " + 5}`; 
-result17.innerHTML = `// ${"  -9  " - 5}`; 
-result18.innerHTML = `// ${null + 1}`; 
-result19.innerHTML = `// ${undefined + 1}`; 
-result20.innerHTML = `// ${" \t \n" - 2}`; 
+function getBinar() {
+	let x = document.getElementById('x').value;
+	let y = document.getElementById('y').value;
+	let result = document.getElementById('result');
+	x= +x;
+	y= +y;
+	let calc = x+y;
+	return result.innerHTML = `Вы сложили числа: ${calc}`;
 }
 
-function correctAdition() {
-let a= prompt("Первое число?", 1);
-let b = prompt("Второе число?", 2);
-result4.innerHTML = `// ${a+b}`; 
-result5.innerHTML = `// ${+a + +b}`; 
-}
+
+
+
+
--- GFM/7/js/script.js
+++ GFM/7/js/script.js
@@ -1,79 +1,50 @@
+
 addEventListener("DOMContentLoaded", onReady); 
 
 function onReady() {
-     document.getElementById('salary').oninput = up
-     document.getElementById('time').oninput = up
-	  document.getElementById('butt').onclick = checkResult;
+    prefixes();
+	 assigning();
+	 typeTransform();
+	 document.getElementById('launchAdd').onclick = correctAdition;
 }
 
-
-
-
-function checkResult() {
-	let salary = document.getElementById('salary').value;
-	let time = document.getElementById('time').value;
-	if (salary == 0) {
-		document.getElementById('salary').value = "";
-		return result.innerHTML = 'То есть хочешь сказать что работал за бесплатно? Печально...';
-	}
-	else if (time == 0) {
-		document.getElementById('time').value = "";
-		return result.innerHTML = 'То есть тебе заплатили просто так? Не верю!';
-}
-	else {
-	getResult();
-	}
+function prefixes(){
+let a = 1, b = 1;
+let d = b++;
+let c = ++a;
+let result1 = document.getElementById('result1');
+let result2 = document.getElementById('result2');
+result1.innerHTML = `// c = ${c} a = ${a}`; 
+result2.innerHTML = `// b = ${b} d = ${d}`;
 }
 
-function up(e1) {
-	let elem = e1.target
-	elem.value = elem.value.replace(/\D/g, '')
+function assigning(){
+	let a = 2;
+	let x = 1 + (a *= 2);
+	result3.innerHTML = `// a = ${a} x = ${x}`; 
 }
 
-
-
-function getResult() {
-	salary = document.getElementById('salary').value;
-	time = document.getElementById('time').value;
-	let result = document.getElementById('result');
-	let calc = salary/time;
-	calc=calc.toFixed(2);
-	let rubName = resultRubName(calc, ['рубль', 'рубля', 'рублей']);
-	let textMessage = alertResult (calc, 
-		["Ммм.. Да ты крут!! Позанимаешься со мной?",
-		"Впринципе неплохо, но определенно есть куда расти!",
-		"Ну для новичка может и сойдет, но я думаю ты достоин большего!",
-		"Что-то как-то мало... Может выгоднее работать за еду?"])
-	return result.innerHTML = `Результат: ваш труд стоил ${calc} ${rubName} в час <br /> <br />  ${textMessage}`;
+function typeTransform() {
+result6.innerHTML = `// ${"" + 1 + 0}`; 
+result7.innerHTML = `// ${"" - 1 + 0}`; 
+result8.innerHTML = `// ${true + false}`; 
+result9.innerHTML = `// ${6 / "3"}`; 
+result10.innerHTML = `// ${"2" * "3"}`; 
+result11.innerHTML = `// ${4 + 5 + "px"}`; 
+result12.innerHTML = `// ${"$" + 4 + 5}`; 
+result13.innerHTML = `// ${"4" - 2}`; 
+result14.innerHTML = `// ${"4px" - 2}`; 
+result15.innerHTML = `// ${7 / 0}`; 
+result16.innerHTML = `// ${"  -9  " + 5}`; 
+result17.innerHTML = `// ${"  -9  " - 5}`; 
+result18.innerHTML = `// ${null + 1}`; 
+result19.innerHTML = `// ${undefined + 1}`; 
+result20.innerHTML = `// ${" \t \n" - 2}`; 
 }
 
-function resultRubName(rubValue, textForms) {
-	rubValue = Math.abs(rubValue) % 100;
-	let rubValueNext = rubValue % 10;
-	if (rubValue > 10 && rubValue < 20) { return textForms[2]; }
-	if (rubValueNext > 1 && rubValueNext < 5) { return textForms[1]; }
-	if (rubValueNext == 1) { return textForms[0]; }
-	return textForms[2];
+function correctAdition() {
+let a= prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+result4.innerHTML = `// ${a+b}`; 
+result5.innerHTML = `// ${+a + +b}`; 
 }
-
-
-
-function alertResult (hourRate, textResult) {
-	 if (hourRate < 50) {
-		return textResult[3];
-	}
-		else if (hourRate > 50 && hourRate < 500) {
-			return textResult[2];
-		}
-			else if (hourRate > 500 && hourRate < 1000) {
-				return textResult[1];
-			}
-				else {
-					return textResult[0];
-				}
-
-}
-
-
-
-
