cbdceae0f6cd5b0e0dfe53085fec3e4d11d6bbf2
Marina Khadieva <utabond89@gmail.com>
2021-10-28 20:37:54 +0300

    add keydown

--- GFM/14.1/js/script.js
+++ GFM/14.1/js/script.js
@@ -1,36 +1,36 @@
 addEventListener("DOMContentLoaded", onReady); 
 
+let x = document.getElementById('opX').value;
+let y = document.getElementById('opY').value;
+let result = document.getElementById('result');
+
 function onReady() {
 	document.getElementById('butt').onclick = Calc;
+	// обработчик нажатия клавиши
+ 	document.addEventListener('keydown', function(e) {
+		if (e.code === 'Enter') {
+			Calc();
+		} else if (e.code === 'ControlLeft') {
+			alert('Поддерживаются следующие математические операторы: Сложение "+", Вычитание "-", Умножение "*", Деление "/", Взятие остатка от деления "%", Возведение в степень "**".');
+		} 
+	 });
+	}
+
 	
-}
+	// обработчик нажатия клавиши в виде функции (для практики)
+	 /* function enterKeyPress(e) {
+		if (e.code === 'Enter') { 
+				Calc();
+			}
+	 } */
 
-/* function EnterKeyPress() {
-	addEventListener('keydown', EnterKeyPress(e))
-		if (e.keyCode === 13) {
-		Calc();
-	 }
- } 
- */
 
-/* function up(e1) {
-	let elem = e1.target
-	elem.value = elem.value.replace(/\D/g, '')
-} */
-
-/* if ((/\D/.test(x)) || (/\D/.test(y)) ) {
-	result.innerHTML = `Программа принимает только числовые значения`;
-	document.getElementById('opX').value = "";
-	document.getElementById('opY').value = "";
-}  */
-
+// Проверка на то что операнды являются числами.	 
 function isNumber(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
  }
 
  function findOperator() {
-	let x = document.getElementById('opX').value;
-	let y = document.getElementById('opY').value;
 	let operator = document.getElementById('op').value;
 	if (operator == "+") {
 		result.innerHTML = `Решение: ${+x + +y}`
@@ -47,14 +47,13 @@ function isNumber(n) {
 	} else if (operator == "**") {
 		result.innerHTML = `Решение: ${x**y}` 
 	} else {
-	result.innerHTML = `<span class="alert">Введенное значение не является рабочим оператором.</span>`;// Для помощи нажмите F1</span>
+	result.innerHTML = `<span class="alert">Введенное значение не является рабочим оператором. Для помощи нажмите левый Ctrl </span>`;
 	}
  }
 
 function Calc() {
 	let x = document.getElementById('opX').value;
 	let y = document.getElementById('opY').value;
-	let result = document.getElementById('result');
 	if (isNumber(x) && isNumber(y)) {
 	findOperator();
 	}
