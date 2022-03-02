7644cb449d7a944758be7e7b29c2975f170577d6
Marina Khadieva <utabond89@gmail.com>
2022-03-02 12:04:12 +0300

    Проблема передачи ответа сервера

---
+++ GFM/53/js/script.js
@@ -0,0 +1,16 @@
+let firstName;
+const serverUrl = 'https://api.genderize.io';
+const url = `${serverUrl}?name=${firstName}`;
+
+
+const buttonSubmit = document.querySelector('button');
+const inputValue = document.querySelector('input');
+const taskResult = document.querySelector('.task__result')
+firstName = inputValue.textContent;
+
+buttonSubmit.addEventListener('click', function (){
+	alert("идет отправка запроса");
+	fetch(url)
+    .then(response => response.text());
+taskResult.textContent = `${firstName} is ${serverAnswer}`;
+})
