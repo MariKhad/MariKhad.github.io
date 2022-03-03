c595f2d667b45261af31f83776a72b00e2e52725
Marina Khadieva <utabond89@gmail.com>
2022-03-03 18:21:56 +0300

    Добавила страну

--- GFM/53/js/script.js
+++ GFM/53/js/script.js
@@ -1,16 +1,41 @@
-let firstName;
-const serverUrl = 'https://api.genderize.io';
-const url = `${serverUrl}?name=${firstName}`;
+let firstName = "";
+const serverUrl1 = 'https://api.genderize.io';
+const serverUrl2 = 'https://api.nationalize.io';
+let serverAnswer = "";
 
 
 const buttonSubmit = document.querySelector('button');
 const inputValue = document.querySelector('input');
 const taskResult = document.querySelector('.task__result')
-firstName = inputValue.textContent;
 
-buttonSubmit.addEventListener('click', function (){
-	alert("идет отправка запроса");
-	fetch(url)
-    .then(response => alert(response.text()));
-//taskResult.textContent = `${firstName} is ${serverAnswer}`;
-})
+
+buttonSubmit.addEventListener('click', async function () {
+	const firstName = inputValue.value;
+	const url1 = `${serverUrl1}?name=${firstName}`;
+	const url2 = `${serverUrl2}?name=${firstName}`;
+
+	let response1 = await fetch(url1);
+
+	if (response1.ok) {
+		let json = await response1.json();
+
+		let response2 = await fetch(url2);
+
+		if (response2.ok) {
+			let json2 = await response2.json();
+
+			taskResult.textContent = `${firstName} is ${json.gender} from ${json2.country[0].country_id}`;
+
+		} else {
+			alert("Ошибка HTTP2: " + response2.status);
+		}
+	} else {
+		alert("Ошибка HTTP1: " + response1.status);
+	}
+
+
+
+
+
+});
+
